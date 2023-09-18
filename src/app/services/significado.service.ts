61685171e252fbf89949989ae571c321f430997d
rodney.sanchez <rodney.sanchez@konecta.com.py>
2023-09-18 18:34:28 -0400

    Metodo para buscar palabara por contenido, servicio de significado agregado

--- src/app/services/significado.service.ts
+++ src/app/services/significado.service.ts
@@ -6,22 +6,22 @@ import { Observable } from "rxjs";
   providedIn: 'root'
 })
 
-export class PalabraService {
+export class SignificadoService {
   private backendUrl = 'http://localhost:8081'; // Reemplaza con la URL de tu backend
 
   constructor(private http: HttpClient) { }
 
-  // Método para registrar un usuario en el backend
-  registrarPalabra(data: any): Observable<any> {
-    const url = `${this.backendUrl}/palabrafrases`;
+  // Método para registrar un significado en el backend
+  registrarSignificado(data: any): Observable<any> {
+    const url = `${this.backendUrl}/significados`;
     return this.http.post(url, data);
   }
-
+  /*
   // Método para obtener el ID de una palabra o frase por su contenido
   getPalabraFraseIdByContenido(contenido: string): Observable<number> {
-    const url = `${this.backendUrl}/palabrafrases/buscarPorContenido/${contenido}`;
+    const url = `${this.backendUrl}/buscarPorContenido/${contenido}`;
     return this.http.get<number>(url);
   }
-
+  */
 }
 
