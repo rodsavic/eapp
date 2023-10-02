a339b02c82e75ee5b6b9b70f5f60f8454394ac4f
rodney.sanchez <rodney.sanchez@konecta.com.py>
2023-10-02 18:02:14 -0300

    componente update sentence creado

--- src/app/pages/sentence/sentence.component.ts
+++ src/app/pages/sentence/sentence.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Sentence } from 'src/app/models/sentence.model';
 import { PalabraService } from 'src/app/services/palabra.service';
 import { SentenceService } from 'src/app/services/sentence.service';
 import { CreateSenteceComponent } from './create-sentece/create-sentece.component';
+import { UpdateSenteceComponent } from './update-sentece/update-sentece.component';
 
 @Component({
   selector: 'app-sentence',
   templateUrl: './sentence.component.html',
   //styleUrls: ['./sentence.component.css']
 })
-export class SentenceComponent implements OnInit{
+export class SentenceComponent implements OnInit {
 
   sentence: any[] = [];
   //idSentence: number;
@@ -19,6 +20,7 @@ export class SentenceComponent implements OnInit{
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialog: MatDialog,
+    private dialogRef: MatDialogRef<SentenceComponent>,
     private palabraService: PalabraService,
     private snack: MatSnackBar,
     private sentenceService: SentenceService,
@@ -52,9 +54,24 @@ export class SentenceComponent implements OnInit{
     });
   }
 
+  // Función para abrir el diálogo de modificar
+  openSentenceUpdateDialog(dIdOracion: number, dTexto: string): void {
+    const dialogRef = this.dialog.open(UpdateSenteceComponent, {
+      width: '40%',
+      height: '40%',
+      data: { id: dIdOracion, palabra: dTexto },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed', result);
+      this.getInitialSenteces();
+    });
+  }
+
+
   deleteSentence(id: number): void {
-    
-    this.sentenceService.deleteSentece(id).subscribe(() => {
+
+    this.sentenceService.deleteSentence(id).subscribe(() => {
       // Actualiza la lista de oraciones después de eliminar
       this.getInitialSenteces();
       // O muestra un mensaje de éxito
@@ -63,5 +80,10 @@ export class SentenceComponent implements OnInit{
       });
     });
   }
-  
+
+  cancelar() {
+    // Cierra el diálogo sin guardar nada
+    this.dialogRef.close();
+
+  }
 }
